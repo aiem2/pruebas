190887c752ed2cbe5cd3953ec431aa618802cc8a
Ignacio Espinosa <emprendiendoem@gmail.com>
2022-04-12 11:44:05 -0400

    Prueba biblioteca

--- 2022.04.05_biblioteca/prueba/biblioteca.sql
+++ 2022.04.05_biblioteca/prueba/biblioteca.sql
@@ -108,3 +108,19 @@ SELECT libro, count(*) FROM prestamo GROUP BY libro;
 
 ---- d. Si se cobrara una multa de $100 por cada día de atraso, mostrar cuánto debería pagar cada usuario que entregue el préstamo después de 7 días.
 SELECT * FROM devolucion WHERE multa > 0;
+
+---DICCIONARIO TABLA
+SELECT
+t1.TABLE_NAME AS tabla_nombre,
+t1.COLUMN_NAME AS columna_nombre,
+t1.ORDINAL_POSITION AS position,
+t1.IS_NULLABLE AS nulo,
+t1.DATA_TYPE AS tipo_dato,
+COALESCE(t1.NUMERIC_PRECISION,
+t1.CHARACTER_MAXIMUM_LENGTH) AS longitud
+FROM
+INFORMATION_SCHEMA.COLUMNS t1
+WHERE
+t1.TABLE_SCHEMA = 'public'
+ORDER BY
+t1.TABLE_NAME, t1.ORDINAL_POSITION;
